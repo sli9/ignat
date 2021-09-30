eb88abcddfeb7d6d44fe4a177764b4b5c6262508
Sil9 <sil9gosy@mail.ru>
2021-09-30 22:53:20 +0300

    9th homework

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,29 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)// stop
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())// setDate
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(!show)// show
     }
     const onMouseLeave = () => {
-        // close
+        setShow(!show)// close
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString()// fix with date  приводит к формату даты на компе
+    const stringDate = date.toLocaleDateString() // fix with date
 
     return (
         <div>
@@ -36,11 +36,12 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
+            {show ?
                 <div>
                     {stringDate}
                 </div>
-            )}
+                : <br/>
+            }
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
