a30ff5d5b32467d79b0db76fd67e5db4189f76ee
Sil9 <sil9gosy@mail.ru>
2021-10-27 22:00:51 +0300

    12th homework

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useCallback} from "react";
 import s from "./HW12.module.css";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import {useDispatch, useSelector} from "react-redux";
+import {changeThemeC} from "./bll/themeReducer";
+import {AppStoreType} from "../h10/bll/store";
 
+export type themeType = 'dark' | 'red' | 'some'
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+    const theme = useSelector<AppStoreType, themeType>(state => state.theme.theme)
 
+    const dispatch = useDispatch()
     // useDispatch, onChangeCallback
 
+    const onChangeCallback = useCallback((theme: themeType) => {
+        dispatch(changeThemeC(theme))
+    }, [dispatch])
     return (
         <div className={s[theme]}>
             <hr/>
@@ -17,7 +26,12 @@ function HW12() {
 
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
-
+            <SuperRadio
+                name={'radio'}
+                options={themes}
+                value={theme}
+                onChangeOption={onChangeCallback}
+            />
             <hr/>
         </div>
     );
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,29 @@
-const initState = {
+import {themeType} from "../HW12";
 
+export type initStateType = typeof initState
+const initState = {
+    theme: 'some' as themeType
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+export const themeReducer = (state: initStateType = initState, action: actionType): initStateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "CHANGE-THEME": {
+            return {...state, theme: action.theme};
         }
-        default: return state;
+        default:
+            return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+type actionType = {
+    type: "CHANGE-THEME"
+    theme: themeType
+}
+
+export const changeThemeC = (theme: themeType): actionType => {
+    return {
+        type: "CHANGE-THEME",
+        theme
+    }
+}; // fix any
